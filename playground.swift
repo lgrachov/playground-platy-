aac4f63433c68bd058b7d3559932d3ca12d20978
lgrachov <78234184+lgrachov@users.noreply.github.com>
2021-12-11 14:23:31 +0200

    Create playground.swift

---
+++ playground.swift
@@ -0,0 +1,620 @@
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
+n if m
+var m = 9
+esle n
+var n = 8
+end
